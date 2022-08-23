524e44379b08a7a305bbd92db935f1fb53211bda
hernando1989 <hernando.munoz.vergara@gmail.com>
2022-08-22 20:17:20 -0400

    Commit test

---
+++ Get Token.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+
+def obtener_token(usuario, clave):
+    url = "https://127.0.0.1:58000/api/v1/ticket"
+    body = {
+            "attributes": {
+                "username": usuario,
+                "password": clave
+            }
+        }
+    cabecera = {
+        "Content-Type": "application/json"
+    }
+    requests.packages.urllib3.disable_warnings()
+    respuesta = requests.post(url, headers=cabecera, data=json.dumps(body), verify=False)
+    token = respuesta.json()['imdata'][0]['attributes']['token']
+    return token
+
+
+token = obtener_token("admin123!", "admin")
+print(token)
